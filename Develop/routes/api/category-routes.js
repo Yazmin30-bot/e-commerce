d8e9af604c97a1d084c800e2532d412faa20bb4d
Yazmin Tinoco <ytip30@gmail.com>
2021-06-09 21:23:48 -0500

    Use Sequelize's  method to show all the products

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,9 +3,24 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  // Use Sequelize's `findAll()` method to show all the products
+  try {
+    const categoryData = await Category.findAll();
+      //{
+      //include: [{ model: Product }],
+      //, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    //});
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
