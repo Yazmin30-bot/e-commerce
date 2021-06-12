ad144373325c0e8ad29b05d27e498ac941501de7
Yazmin Tinoco <ytip30@gmail.com>
2021-06-11 21:08:52 -0500

    Use Sequelize's  method to DELETE the product by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -115,8 +115,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  //Use Sequelize's `destroy()` method to DELETE the product by id
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
