cb4495725389f991a9281433f4236630219b338b
Yazmin Tinoco <ytip30@gmail.com>
2021-06-11 21:01:26 -0500

    Use Sequelize's  method to show the product by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -20,10 +20,26 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+
+  router.get('/:id', async (req, res) => {
+    // find a single product by its `id`
+    // be sure to include its associated Category and Tag data
+    // Use Sequelize's `findByPk()` method to show the product by id
+    try {
+      const productData = await Product.findByPk(req.params.id);
+      /*   , {
+        include: [{ model: Category }], //, { model: Tag }
+      }); */
+      if (!productData) {
+        res.status(404).json({ message: 'No product found with this id!' });
+        return;
+      }
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 // create new product
 router.post('/', (req, res) => {
