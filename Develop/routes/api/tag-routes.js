42f00b93d586cbd4c0a07f0f0fa2e00083a2a6fc
Yazmin Tinoco <ytip30@gmail.com>
2021-06-11 22:41:22 -0500

    Use Sequelize's  method to CREATE a new tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -22,7 +22,7 @@ router.get('/', async(req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  // Use Sequelize's `findAll()` method to show the tag by id
+  // Use Sequelize's `findByPk()` method to show the tag by id
   try{
     const tagData = await Tag.findByPk(req.params.id,{
       include: [{ model: Product, through: ProductTag, as: 'tag_products'}],
@@ -39,6 +39,13 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  //Use Sequelize's `CREATE()` method to CREATE a new tag 
+  try{
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
