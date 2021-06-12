992b69f22b9be731ae621a17bcba6177ea6939e0
Yazmin Tinoco <ytip30@gmail.com>
2021-06-11 22:36:37 -0500

    Use Sequelize's  method to show all the tags

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -10,12 +10,7 @@ router.get('/', async (req, res) => {
   // Use Sequelize's `findAll()` method to show all the products
   try{
     const productData = await Product.findAll({
-      include: [
-        Category,
-         { model: Tag,
-        through: ProductTag,
-       },
-      ],
+      include: [{model: Category},{model: Tag, through: ProductTag}],
     });
     res.status(200).json(productData);
   }catch(err){
@@ -30,10 +25,10 @@ router.get('/', async (req, res) => {
     // be sure to include its associated Category and Tag data
     // Use Sequelize's `findByPk()` method to show the product by id
     try {
-      const productData = await Product.findByPk(req.params.id);
-      /*   , {
-        include: [{ model: Category }], //, { model: Tag }
-      }); */
+      const productData = await Product.findByPk(req.params.id,
+          {include: [{ model: Category }, { model: Tag }],
+       
+      }); 
       if (!productData) {
         res.status(404).json({ message: 'No product found with this id!' });
         return;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,6 +6,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  // Use Sequelize's `findAll()` method to show all the tags
+  try{
+    const tagData = await Tag.findAll({
+      include: [{ model: Product},{ model: ProductTag}],
+      //, through: ProductTag, as: 'tag_products'
+    });
+    res.status(200).json(tagData);
+
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
