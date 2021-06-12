50e4b8304c8469fa2c8f53cab842a86aca0a6504
Yazmin Tinoco <ytip30@gmail.com>
2021-06-11 22:38:59 -0500

    Use Sequelize's  method to show the tag by id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,7 +3,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all tags
   // be sure to include its associated Product data
   // Use Sequelize's `findAll()` method to show all the tags
@@ -19,9 +19,22 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  // Use Sequelize's `findAll()` method to show the tag by id
+  try{
+    const tagData = await Tag.findByPk(req.params.id,{
+      include: [{ model: Product, through: ProductTag, as: 'tag_products'}],
+    })
+    if(!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
