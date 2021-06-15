98542bed7bd5e5e27d97d4da3f34822a4f688307
Yazmin Tinoco <ytip30@gmail.com>
2021-06-14 20:18:31 -0500

    Added the foreignKey property to bind to their respective 'X_id' in the ProductTag.js model

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,18 +3,18 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   // Use Sequelize's `findAll()` method to show all the tags
-  try{
+  try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag, as: 'product_data'}],
+      include: [{ model: Product, through: ProductTag, as: 'product_data' }],
       //, through: ProductTag, as: 'tag_products'
     });
     res.status(200).json(tagData);
 
-  }catch(err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
@@ -23,16 +23,16 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   // Use Sequelize's `findByPk()` method to show the tag by id
-  try{
-    const tagData = await Tag.findByPk(req.params.id,{
-      include: [{ model: Product, through: ProductTag, as: 'product_data'}],
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'product_data' }],
     })
-    if(!tagData) {
+    if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
       return;
     }
     res.status(200).json(tagData);
-  }catch(err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
@@ -40,10 +40,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   //Use Sequelize's `CREATE()` method to CREATE a new tag 
-  try{
+  try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
-  }catch(err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
@@ -51,39 +51,39 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   //Use Sequelize's `update()` method to update the tag by id
-  try{
-    const tagData = await Tag.update(req.body,{
+  try {
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id
       }
     });
-    if(!tagData) {
+    if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
       return;
     }
     res.status(200).json(tagData);
 
-  }catch(err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.delete('/:id',async (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   //Use Sequelize's `destroy()` method to delete the tag by id
-  try{
+  try {
     const tagData = await Tag.destroy({
       where: {
         id: req.params.id
       }
     });
-    if(!tagData) {
+    if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
       return;
     }
     res.status(200).json(tagData);
 
-  }catch(err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
